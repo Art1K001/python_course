5491884cc13657f422f1f7cb892250ac297b6d25
Art1K001 <artik.bil@gmail.com>
2024-09-10 13:51:57 +0300

    add task 9-9

---
+++ classes_9/do_it_yourself/upgrade_batteries_9-9.py
@@ -0,0 +1,81 @@
+class Car:
+    """ Проста спроба змоделювати машину """
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        """ Повернути відформатоване змістовне імʼя """
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()
+
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it")
+
+    def update_odometer(self, mileage):
+        """
+        Задати значення одометра
+        Відкинути зміну в разі спроби відмотати показники одометра
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer")
+
+    def increment_odometer(self, miles):
+        """
+        Додати задане значення до показників одометра
+        """
+        self.odometer_reading += miles
+
+class Battery:
+    """Проста спроба змоделювати акумулятор електрокара"""
+    def __init__(self, battery_size=75):
+        """ Ініціалізувати атрибути акумулятора """
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """ Вивести повідомлення про розмір акумулятора """
+        print(f"This car has a {self.battery_size} kWh battery")
+
+    def get_range(self):
+        """
+        Вивести повідомлення про відстань
+        яку може подалати авто відповідно
+        до ємності акумулятора
+        """
+        if self.battery_size == 75:
+            range = 260
+        elif self.battery_size == 100:
+            range = 315
+
+        print(f"This car can go about {range} miles on a full charge")
+
+    def upgrade_battery(self):
+        if self.battery_size < 100:
+            self.battery_size = 100
+            print("Battery upgade to max")
+
+class ElectricCar(Car):
+    """ Змоделювати властивості, притаманні електрокарами """
+    """ Ініціалізувати атрибути акумулятора """
+    def __init__(self, make, model, year):
+        """
+        Ініціалізувати атрибути батьківського класу
+        Тоді ініалізувати атрибути електрокара
+        """
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        """ Електрокари не мають бензобаків """
+        print(f"This car doesn't need a gas tank")
+
+my_tesla = ElectricCar("tesla", "model s", "2019")
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
+my_tesla.battery.upgrade_battery()
+my_tesla.battery.get_range()
