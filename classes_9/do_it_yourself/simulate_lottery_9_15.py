3bf975869cfe2e779602f346f2523111bfe68908
Art1K001 <artik.bil@gmail.com>
2024-09-16 15:17:24 +0300

    created branch task_9_15

--- classes_9/do_it_yourself/simulate_lottery_9_15.py
+++ classes_9/do_it_yourself/simulate_lottery_9_15.py
@@ -1,18 +1,20 @@
 import random
 
-def generate_ticket(my_ticket):
+def generate_ticket():
     my_tuple = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E")
+    return random.sample(my_tuple, 4)
 
+def simulate_lottery(ticket):
     attempts = 0
     while True:
         attempts += 1
-        lottery_ticket = random.sample(my_tuple, 4)
-        if lottery_ticket == my_ticket:
+        lottery_ticket = generate_ticket()
+        if lottery_ticket == ticket:
             break
     return attempts
 
 my_ticket = ["A", 2, 6, "B"]
-attempts = generate_ticket(my_ticket)
+attempts = simulate_lottery(my_ticket)
 print(f"After {attempts} tries, you win")
 
 
